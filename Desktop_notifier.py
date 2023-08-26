16dd7f2af3ddd423227f613f5a3acf85a914005d
Shriya Srivastava <100456530+shriyaa01@users.noreply.github.com>
2023-08-26 19:18:46 +0530

    Initial commit

--- Desktop_notifier.py
+++ Desktop_notifier.py
@@ -1,12 +1,14 @@
-from plyer import notification as np
+# Importing the required libraries
+from plyer import notification as np  # Importing the notification module
 import time
 
-
-if __name__=='__main__':
-
+# Main program loop
+if __name__ == '__main__':
     while True:
-        np.notify(title="Keep Doing",
-                  message="If you want to shine like a sun, first burn like a sun.",
-                  timeout=30
-                  )
-        time.sleep(3600)
+        np.notify(
+            title="Keep Doing",  # Title of the notification
+            message="If you want to shine like a sun, first burn like a sun.",  # Notification message
+            timeout=30  # Timeout (in seconds) for how long the notification should be visible
+        )
+        time.sleep(3600)  # Wait for 1 hour before sending the next notification
+
